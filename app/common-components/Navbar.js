02808ce54f9dafbca72ae1b13f93faaed7c8829c
Nahid-07 <nahidasif1998@gmail.com>
2023-05-18 00:25:20 +0600

    desktop navbar added

--- app/common-components/Navbar.js
+++ app/common-components/Navbar.js
@@ -1,9 +1,22 @@
 const Navbar = () => {
-    return (
-        <nav>
-            
-        </nav>
-    );
+  return (
+    <div className="flex justify-between p-6 h-14 shadow-md items-center">
+      <header>
+        <h1 className="text-2xl font-semibold">Resale.Bd</h1>
+      </header>
+      <nav>
+        <div>
+          <ul className="flex space-x-6 text-lg">
+            <li>Home</li>
+            <li>Dashboard</li>
+            <li>Blog</li>
+            <li>Log in</li>
+            <li>Log out</li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
