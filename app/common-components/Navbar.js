807abe45cf4b12506e9672e29dbf03fc9cc5efce
Nahid-07 <nahidasif1998@gmail.com>
2023-05-18 00:51:53 +0600

    mobile navbar added

--- app/common-components/Navbar.js
+++ app/common-components/Navbar.js
@@ -1,21 +1,47 @@
+"use client";
+import { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="flex justify-between p-6 h-14 shadow-md items-center">
-      <header>
-        <h1 className="text-2xl font-semibold">Resale.Bd</h1>
-      </header>
-      <nav>
-        <div>
-          <ul className="flex space-x-6 text-lg">
-            <li>Home</li>
-            <li>Dashboard</li>
-            <li>Blog</li>
-            <li>Log in</li>
-            <li>Log out</li>
-          </ul>
+    <>
+      <div className="md:flex justify-between p-6 h-14 shadow-md items-center hidden">
+        <header>
+          <h1 className="text-2xl font-semibold">Resale.Bd</h1>
+        </header>
+        <nav>
+          <div>
+            <ul className="flex space-x-6 text-lg">
+              <li>Home</li>
+              <li>Dashboard</li>
+              <li>Blog</li>
+              <li>Log in</li>
+              <li>Log out</li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+
+      {/* navbar for mobile device */}
+
+      <div className="p-6 flex md:hidden justify-between h-10 shadow-md items-center">
+        <header>Resale.Bd</header>
+        {/* tap and show menu icon */}
+        <div onClick={() => setOpen(!open)}>
+          <RxHamburgerMenu />
         </div>
+      </div>
+
+      <nav className={`${open ? "block" : "hidden"} bg-gray-100 `}>
+        <ul className="p-6 space-y-4">
+          <li>Home</li>
+          <li>Dashboard</li>
+          <li>Blog</li>
+          <li>Log in</li>
+          <li>Log out</li>
+        </ul>
       </nav>
-    </div>
+    </>
   );
 };
 
