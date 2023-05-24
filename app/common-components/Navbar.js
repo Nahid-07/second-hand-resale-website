9e93980fc48363d30569e53f147ba9ac2044e3d4
Nahid-07 <nahidasif1998@gmail.com>
2023-05-24 22:49:50 +0600

    added file based route

--- app/common-components/Navbar.js
+++ app/common-components/Navbar.js
@@ -1,11 +1,12 @@
 "use client";
+import Link from "next/link";
 import { useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   return (
     <>
-    {/* desktop navbar */}
+      {/* desktop navbar */}
       <div className="md:flex justify-between p-6 h-14 shadow-md items-center hidden">
         <header>
           <h1 className="text-2xl font-semibold">Resale.Bd</h1>
@@ -13,11 +14,21 @@ const Navbar = () => {
         <nav>
           <div>
             <ul className="flex space-x-6 text-lg">
-              <li>Home</li>
-              <li>Dashboard</li>
-              <li>Blog</li>
-              <li>Log in</li>
-              <li>Log out</li>
+              <Link href="/">
+                <li>Home</li>
+              </Link>
+              <Link href="/dashbord">
+                <li>Dashboard</li>
+              </Link>
+              <Link href="/blog">
+                <li>Blog</li>
+              </Link>
+              <Link href="/SignIn">
+                <li>Log in</li>
+              </Link>
+              <Link href="/">
+                <li>Log out</li>
+              </Link>
             </ul>
           </div>
         </nav>
@@ -35,11 +46,21 @@ const Navbar = () => {
 
       <nav className={`${open ? "block" : "hidden"} bg-gray-100 `}>
         <ul className="p-6 space-y-4">
-          <li>Home</li>
-          <li>Dashboard</li>
-          <li>Blog</li>
-          <li>Log in</li>
-          <li>Log out</li>
+          <Link href="/">
+            <li>Home</li>
+          </Link>
+          <Link href="/dashbord">
+            <li>Dashboard</li>
+          </Link>
+          <Link href="/blog">
+            <li>Blog</li>
+          </Link>
+          <Link href="/SignIn">
+            <li>Log in</li>
+          </Link>
+          <Link href="/">
+            <li>Log out</li>
+          </Link>
         </ul>
       </nav>
     </>
